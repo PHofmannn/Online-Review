f5f3a6fecd852a21652566aeec14e1e6fe611598
PHofmannn <132584414+PHofmannn@users.noreply.github.com>
2024-04-18 18:41:38 +0200

    Updated code

--- Data Preperation/JoinMetaData.py
+++ Data Preperation/JoinMetaData.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np
 
 # Import the Review data
-file_path1 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/data_utilitarian.csv'
+file_path1 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/Data Preperation/data_utilitarian_filter.csv'
 data_utilitarian = pd.read_csv(file_path1)
 
 
 # Importing Meta Data
-file_path2 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/top_200_products.csv'
+file_path2 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/Selecting Data/Appliance/top_200_products_Appliances.'
 
 # Columns to import
 columns_to_import = ['main_category', 'title', 'average_rating', 'rating_number', 'features', 'price', 'parent_asin']
@@ -19,7 +19,7 @@ data_meta = pd.read_csv(file_path2, usecols=columns_to_import)
 
 
 # Merge the data based on parent_asin
-merged_data = pd.merge(data_utilitarian, data_meta, on='parent_asin', how='left')
+meta_data_Utilitarian_Filter = pd.merge(data_utilitarian, data_meta, on='parent_asin', how='left')
 
 # Display the merged data
 print(merged_data.head(3))
