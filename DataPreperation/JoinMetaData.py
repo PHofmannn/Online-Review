c508345c5035f6cb4bba6947e1dab3b35f7d688f
PHofmannn <132584414+PHofmannn@users.noreply.github.com>
2024-04-19 12:15:58 +0200

    Updated Data

--- DataPreperation/JoinMetaData.py
+++ DataPreperation/JoinMetaData.py
@@ -1,18 +1,15 @@
-import sys
-print(sys.executable)
-
 # Matching Data with Metadata according to the Parent Asin
 
 import pandas as pd
 import numpy as np
 
 # Import the Review data
-file_path1 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/Data Preperation/data_utilitarian_filter.csv'
+file_path1 = '/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/data_utilitarian_filter.csv'
 data_utilitarian = pd.read_csv(file_path1)
 
 
 # Importing Meta Data
-file_path2 = '/Users/paulahofmann/Library/CloudStorage/OneDrive-Persönlich/Uni/2. Lernpools Master/Masterarbeit/Coding/Online-Review/Selecting Data/Appliance/top_200_products_Appliances.'
+file_path2 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/Appliance/top_200_products_Appliances.csv'
 
 # Columns to import
 columns_to_import = ['main_category', 'title', 'average_rating', 'rating_number', 'features', 'price', 'parent_asin']
@@ -24,4 +21,8 @@ data_meta = pd.read_csv(file_path2, usecols=columns_to_import)
 # Merge the data based on parent_asin
 meta_data_Utilitarian_Filter = pd.merge(data_utilitarian, data_meta, on='parent_asin', how='left')
 
-# Display the merg
+# Display the merged Data
+print (meta_data_Utilitarian_Filter.head(3))
+
+# Save the data
+meta_data_Utilitarian_Filter.to_csv('/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/meta_data_utilitarian_filter.csv', index=False)
