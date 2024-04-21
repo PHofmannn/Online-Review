01293b3011e762d6564e188e0a65399fd2d2fff5
PHofmannn <132584414+PHofmannn@users.noreply.github.com>
2024-04-21 09:17:20 +0200

    Update

--- DataPreperation/JoinMetaData.py
+++ DataPreperation/JoinMetaData.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np
 
 # Import the Review data
-file_path1 = '/Users/paulahofmann/Documents/Coding/Online-Review/DataBase/Without Meta/data_hedonic_Cleaned.csv'
+file_path1 = '/Volumes/Paula_SSD/Data/JsonLine/filtered_Mouse.json'
 data_review= pd.read_csv(file_path1)
 
 
 # Importing Meta Data
-file_path2 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/Beauty & Care/top_200_products_BeautyCare.csv'
+file_path2 = 'Online-Review/SelectingData/VideoGames/top_200_products_VideoGames.csv'
 
 # Columns to import
 columns_to_import = ['main_category', 'title', 'average_rating', 'rating_number', 'features', 'price', 'parent_asin']
@@ -25,4 +25,4 @@ meta_data_Review = pd.merge(data_review, data_meta, on='parent_asin', how='left'
 print (meta_data_Review.head(3))
 
 # Save the data, please modify the name of the file
-meta_data_Review.to_csv('/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/Meta_hedonic.csv', index=False)
+meta_data_Review.to_csv('/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/Meta_Mouse.csv', index=False)
