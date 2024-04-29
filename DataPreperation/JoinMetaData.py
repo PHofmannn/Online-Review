e17b18dd30e7be76a94fa9a67d079d794435bf18
PHofmannn <132584414+PHofmannn@users.noreply.github.com>
2024-04-29 16:53:40 +0200

    ‚‚‚

--- DataPreperation/JoinMetaData.py
+++ DataPreperation/JoinMetaData.py
@@ -4,12 +4,12 @@ import pandas as pd
 import numpy as np
 
 # Import the Review data
-file_path1 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/Health&Household/merged_Utilitarian.csv'
+file_path1 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/filtered_Vinyl.csv'
 data_review= pd.read_csv(file_path1)
 
 
 # Importing Meta Data
-file_path2 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/Health&Household/top_200_products_Household.csv'
+file_path2 = '/Users/paulahofmann/Documents/Coding/Online-Review/SelectingData/top_200_products_MusicVinyl.csv'
 
 # Columns to import
 columns_to_import = ['main_category', 'title', 'average_rating', 'rating_number', 'features', 'price', 'parent_asin']
@@ -25,6 +25,6 @@ meta_data_Review = pd.merge(data_review, data_meta, on='parent_asin', how='left'
 print (meta_data_Review.head(3))
 
 # Save the data, please modify the name of the file
-meta_data_Review.to_csv('/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/Utilitarian_Household.csv', index=False)
+meta_data_Review.to_csv('/Users/paulahofmann/Documents/Coding/Online-Review/DataPreperation/Vinyl_Music.csv', index=False)
 
 print(data_review.columns)
